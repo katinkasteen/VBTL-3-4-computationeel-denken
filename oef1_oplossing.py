4eb40f852350c3ee98d1cb0220ffeea1344f42df
katinkasteen <86676690+katinkasteen@users.noreply.github.com>
2021-06-30 08:55:57 +0200

    Add files via upload

---
+++ oef1_oplossing.py
@@ -0,0 +1,7 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> zijde = int(input("Voer de lengte van een zijde van een vierkant in (zonder eenheden)."))
+omtrek = 4*zijde
+oppervlakte = zijde * zijde
+print("De omtrek is", omtrek)
+print("De oppervlakte is", oppervlakte)
